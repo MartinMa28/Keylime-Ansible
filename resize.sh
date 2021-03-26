25e9cdcaf6c6f8046f831f010cc49dbf58ded202
Yilin Ma <1135485226@qq.com>
2021-03-25 23:26:21 -0700

    fix variable name

--- resize.sh
+++ resize.sh
@@ -4,5 +4,5 @@ echo "Bash version ${BASH_VERSION}"
 for server_idx in {1..29}
 do
   echo "${server_idx}"
-  openstack server resize "keylime_ansible_agent_35-${ip_addr}" --flavor m1.keylime
+  openstack server resize "keylime_ansible_agent_35-${server_idx}" --flavor m1.keylime
 done
--- resize_confirm.sh
+++ resize_confirm.sh
@@ -4,5 +4,5 @@ echo "Bash version ${BASH_VERSION}"
 for server_idx in {1..29}
 do
   echo "${server_idx}"
-  openstack server resize confirm "keylime_ansible_agent_35-${ip_addr}"
+  openstack server resize confirm "keylime_ansible_agent_35-${server_idx}"
 done
